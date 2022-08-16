3e26ad76c5c7f4968526af4082985b12e3f5f835
Hossam Tuwano <hossamtuwano@gmail.com>
2022-08-16 08:16:05 +0300

    initial commit

---
+++ category/models.py
@@ -0,0 +1,24 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
+from tokenize import blank_re
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    decription =  models.CharField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+
+
+        
