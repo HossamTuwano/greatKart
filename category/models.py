e70c4312cb372d06c420aea9398ac35102621b04
Hossam Tuwano <hossamtuwano@gmail.com>
2022-08-16 19:42:15 +0300

    get_product_by_category

--- category/models.py
+++ category/models.py
@@ -3,6 +3,7 @@ from tabnanny import verbose
 from tokenize import blank_re
 from unicodedata import category
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -16,6 +17,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
 
     def __str__(self):
         return self.category_name
