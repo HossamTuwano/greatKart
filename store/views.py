54a60af3ed8c6436e1b51e336e2ff5ed2a3ab70a
Hossam Tuwano <hossamtuwano@gmail.com>
2022-08-16 19:17:38 +0300

    added categories route

--- store/views.py
+++ store/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
-from ..category.models import Category
+from category.models import Category
 from .models import Product
 
 # Create your views here.
@@ -10,9 +9,13 @@ def store(request, category_slug=None):
     products = None
 
     if category_slug != None:
-        categories = get_object_or_404(Category, category_slug=slug)
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     context = {
         'products' : products,
